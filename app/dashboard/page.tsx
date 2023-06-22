953feeaf73bf4f8b6b76be20b51d59f7724ca2d3
Ryan Huang <11435382+ryanhuangio@users.noreply.github.com>
2023-06-21 18:28:33 -0700

    Added Tailwind styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,25 +24,25 @@ export default function Dashboard() {
         onClick={toggleNavbar}
         className={`fixed top-0 left-0 h-screen 
         ${navbarExpanded ? 'w-64' : 'w-16'}
-         flex flex-col bg-slate-300 dark:bg-slate-800 shadow-lg text-slate-900 dark:text-white `}
+         flex flex-col bg-slate-300 dark:bg-slate-800 shadow-lg text-slate-900 dark:text-white p-3 `}
       >
-        <a>
-          <FontAwesomeIcon icon={faGauge} />
-          <span className={`${navbarExpanded ? 'visible' : 'invisible'}`}>Dashboard</span>
+        <a className='align-middle mb-4 flex items-center'>
+          <FontAwesomeIcon icon={faGauge} size='2x' className=' mr-3' />
+          <span className={`${navbarExpanded ? 'inline' : 'hidden'}`}>Dashboard</span>
           </a>
-        <a>
-          <FontAwesomeIcon icon={faRightFromBracket} />
-          <span className={`${navbarExpanded ? 'visible' : 'invisible'}`}>Logout</span>
+        <a className='align-middle mb-4 flex items-center'>
+          <FontAwesomeIcon icon={faRightFromBracket}  size='2x' className=' mr-3' />
+          <span className={`${navbarExpanded ? 'inline' : 'hidden'}`}>Logout</span>
           </a>
         
       </div>
-      <div className={`bg-slate-200 dark:bg-slate-500 text-black dark:text-white block w-screen h-screen flex-col transition-all
+      <div className={`bg-slate-200 dark:bg-slate-500 text-black dark:text-white block w-screen h-screen flex-col transition-all pl-8 pr-8 pt-4 pb-4
         ${navbarExpanded ? 'ml-64' : 'ml-16'}
       `}>
         <a onClick={toggleDarkMode} className='rounded-full border-slate-700 inline-block p-2'>
-        <FontAwesomeIcon icon={faMoon} />
+        <FontAwesomeIcon icon={faMoon}  size='1x' />
         </a>
-        <h1>Dashboard</h1>
+        <h1 className='text-3xl font-bold'>Dashboard</h1>
         <p>dashboard contents...</p>
       </div>
     </div>
